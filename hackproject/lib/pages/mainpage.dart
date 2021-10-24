5824890cdce04180745f7c2553af6b757e5582f2
IanLu <plu4@uw.edu>
2021-10-23 17:52:33 -0700

    test addition

--- hackproject/lib/pages/mainpage.dart
+++ hackproject/lib/pages/mainpage.dart
@@ -12,20 +12,36 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     Size _size = MediaQuery.of(context).size;
     return Center(
-      child: Container(
-        width: _size.width,
-        height: _size.height,
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(30)),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black,
-                offset: Offset(0, -10),
-                blurRadius: 20,
-                spreadRadius: 0)
-          ],
-          color: Color(0xffD37373),
-        ),
+      child: Column(
+        children: [
+          Container(child: Text("")),
+          Container(
+            width: _size.width,
+            height: _size.height,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black,
+                    offset: Offset(0, -10),
+                    blurRadius: 20,
+                    spreadRadius: 0)
+              ],
+              color: Color(0xffD37373),
+            ),
+          ),
+          // Container(
+          //   alignment: Alignment.center,
+          //   child: TextButton(onPressed: () {
+          //     Navigator.push(
+          //       context,
+          //       MaterialPageRoute(
+          //         builder: (context) => null,
+          //       ),
+          //     );
+          //   }, child: Container(child: Text(),),)
+          // )
+        ],
       ),
     );
   }
